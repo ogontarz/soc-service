9a42c47562da00439e7e0dc4beea05142524465d
Ola <you@example.com>
2018-08-23 14:11:03 +0200

    Add simple debug logging & enpoint for getting service stats

--- index.js
+++ index.js
@@ -3,67 +3,62 @@
 const express = require("express");
 const Ajv = require("ajv");
 const bodyParser = require("body-parser");
-//const signer = require("./signer.js");
+require('log-timestamp');
 
 
+const signer = require("./signer.js");
 const {Queue, QueueConsumer} = require("./queue");
 const ElasticConsumer = require("./elastic-consumer");
 const SyslogConsumer = require("./syslog-consumer");
 
+let elasticQueue = new Queue(20, 2000);
+let syslogQueue = new Queue(20, 2000);
 
-let elasticQueue = new Queue(10, 200);
-let syslogQueue = new Queue(30, 500);
+let useElastic = process.env.elastic == "true";
+let useSyslog = process.env.syslog == "true";
 
 
-let config = require("./config.json");
-
-let useElastic = config.elastic.enable;
-if (useElastic == undefined) {
-    console.log("enable/disable connection to elasticsearch service");
-    process.exit(1);
-} 
-if (useElastic == true) {
-    if (config.elastic.ip == undefined || config.elastic.port == undefined) {
-        console.log("set proper values for elastic ip and port");
-        process.exit(1);
-    }
-    let elasticConsumer = new ElasticConsumer(config.elastic.ip, config.elastic.port); 
+if (useElastic) {
+    let elasticConsumer = new ElasticConsumer(process.env.elastic_ip, process.env.elastic_port); 
     elasticQueue.registerConsumer(elasticConsumer);
-    console.log("Elasticsearch server configuraion ok");
+    console.log("Elasticsearch server configuraion OK on " + process.env.elastic_ip + ":" + process.env.elastic_port);
 } 
-
-let useSyslog = config.syslog.enable;
-if (useSyslog == undefined) {
-    console.log("enable/disable connection to syslog server");
-    process.exit(1);
-}
-if (useSyslog == true) {
-    if (config.syslog.ip == undefined || config.syslog.port == undefined) {
-        console.log("set proper values for syslog server ip and TCP port");
-        process.exit(1);
-    }
-    let syslogConsumer = new SyslogConsumer(config.syslog.ip, config.syslog.port);
+if (useSyslog) {
+    let syslogConsumer = new SyslogConsumer(process.env.syslog_ip, process.env.syslog_port);
     syslogQueue.registerConsumer(syslogConsumer);
-    console.log("Syslog server configuration ok");
+    console.log("Syslog server configuration OK on " + process.env.syslog_ip + ":" + process.env.syslog_port);
 }
 
+
+const debug = process.env.env == "debug";
+const port = process.env.port || 3000;
 
 const app = express();
 
-// get port value from command line argument
-const args = process.argv.slice(2);
-const port = args[0] || 3000;
 
+let totalRequests = 0;
+let validatedRequests = 0;
 
 let schema = require("./schema.json")
 let ajv = new Ajv({ verbose: true });
 let validate = ajv.compile(schema);
 
 
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
+function jsonParseError(err, req, res, next) {
+    if (err instanceof SyntaxError && err.type == 'entity.parse.failed') {
+        res.status(400).send("body of request is not a JSON");
+    } else if (err instanceof Error && err.type === 'entity.too.large') {
+        res.status(400).send("body too large");
+    } else {
+        res.status(500).send();
+    }
+}
+
+
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(express.json({ limit: "200000"}));
+app.use(jsonParseError);
 
 
 app.get("/", (request, response) => {
@@ -74,59 +69,52 @@ app.get("/", (request, response) => {
 });
 
 
-app.post("/events", (req, res) => {
-    let json = req.body; 
-    
+app.post("/events", (request, response) => {
+    let json = request.body; 
+    totalRequests++;
+
+    if (debug) {
+        console.log("Received new event, total events: " + totalRequests);
+    }
+
     // if event comes from html form, extract textarea input
     if (json.hasOwnProperty("value")) {
         json = JSON.parse(json.value);
     }
 
-    let isValid = validate(json);
-    if (isValid) {
-        res.statusCode = 200;
-        res.send("OK");  
-        //let hmac = signer.sign(req.body, "secret");
-
+    if (validate(json)) {
+        response.statusCode = 200;
+        response.send("OK");  
+        
+        if (debug) {
+            console.log("Event validated correctly");
+        }
+        
         if (useElastic) {
             elasticQueue.addToQueue(json);
+            if (debug) {
+                console.log("Adding event to the elastic consumer queue");
+            }
         }
         if (useSyslog) {
             syslogQueue.addToQueue(json);
+            if (debug) {
+                console.log("Adding event to the syslog consumer queue");
+            }
         }
+
+        let hmac = signer.sign(request.body, "secret");
+        validatedRequests++;
+
     } else {
-        res.statusCode = 400;
-        res.json({ error: "Invalid json format"});
+        response.statusCode = 400;
+        response.json({ error: "Invalid json format"});
+        
+        if (debug) {
+            console.log("Wrong event format, event ignored");
+        }
     }
 });
-
-
-// app.get("/events", (request, response) => {
-//     if (request.query.start) {
-//         elastic.searchAfter(request.query.start).then((results) => {
-//             let obj = formatResponse(results);            
-//             response.json(obj);
-//             //console.log(JSON.stringify(obj));
-//         });
-//     } else {
-//         // no parameters get
-//         elastic.search().then((results) => {
-//             let obj = formatResponse(results);
-//             response.json(obj);
-//             //console.log(JSON.stringify(obj));
-//         });
-//     }
-// });
-
-// function formatResponse(results) {
-//     let r = results.hits.hits.map((hit) => {
-//         return hit._source;
-//     });
-//     let obj = {};
-//     let key = "events";
-//     obj[key] = r;
-//     return obj;
-// }
 
 
 app.get("/schema", (request, response) => {
@@ -134,11 +122,18 @@ app.get("/schema", (request, response) => {
     response.json(schema);
 });
 
+
 app.post("/schema", (request, response) => {
     response.statusCode = 200;
     response.send("OK");
     schema = request.body;
     validate = ajv.compile(schema);
+});
+
+
+app.get("/stats", (request, response) => {
+    response.statusCode = 200;
+    response.json({ "Received events" : totalRequests, "Valid events" : validatedRequests });
 });
 
 
